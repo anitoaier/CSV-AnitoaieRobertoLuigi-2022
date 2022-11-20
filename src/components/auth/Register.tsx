dea109a151fcc3aea8c8ab09e59fb26c4e450199
RobertoLuigi <anitoaier@gmail.com>
2022-11-20 21:37:48 +0100

    Curs3

--- src/components/auth/Register.tsx
+++ src/components/auth/Register.tsx
@@ -2,10 +2,14 @@ import React, { FC, SyntheticEvent, Dispatch, SetStateAction } from "react";
 import { AuthModals } from "./types";
 import { UserTypes } from "./types";
 import './auth-styles.css';
+import { useUserAuth } from "../../contexts/AuthContext";
 
 const Register: FC<{ setModalOpen: Dispatch<SetStateAction<AuthModals>>; }> = ({
     setModalOpen,
 }) => {
+
+    const { signUp } = useUserAuth();
+
     const onSubmit = async (e: SyntheticEvent) => {
         try {
             e.preventDefault();
@@ -15,9 +19,10 @@ const Register: FC<{ setModalOpen: Dispatch<SetStateAction<AuthModals>>; }> = ({
             console.log(target);
             const email = target.email.value;
             const password = target.password.value;
-            const name = target["name"].valueOf;
+            const name = target["name"].value;
             const phoneNumber = target.phoneNumber.value;
             const userType = target["user-type-radio"].value;
+            await signUp(email,password,phoneNumber,name,userType);
             // Custom validation
             if (password.length < 5) {
                 alert('Password is too short');
